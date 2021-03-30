070ad53fa8789f9ebf8a65f684996724ebb673ee
mptelepro <72145569+mptelepro@users.noreply.github.com>
2021-03-30 21:05:14 +0530

    Update __main__.py

--- DaisyX/__main__.py
+++ DaisyX/__main__.py
@@ -835,7 +835,6 @@ def main():
 
     # dispatcher.add_handler(test_handler)
     dispatcher.add_handler(start_handler)
-    dispatcher.add_handler(aboutmanu_handler)
     dispatcher.add_handler(about_callback_handler)
     dispatcher.add_handler(help_handler)
     dispatcher.add_handler(settings_handler)
